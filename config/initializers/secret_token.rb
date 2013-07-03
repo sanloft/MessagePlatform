562a290d1820525a9b9a0a7ebb3e7c1345cbfdb0
sanloft <sanloft@gmail.com>
2013-07-03 16:48:11 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MessagePlatform::Application.config.secret_token = '24d3848990e4f8758384b92729c00904ba9a17cbfbf7793468db12a68ba912a21c9d4fa74f1b09283a774b99059e73aface9adbcb46f196aaa3333e13c81c400'
